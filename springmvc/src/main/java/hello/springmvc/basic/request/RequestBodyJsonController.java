3436c0f67f748c4613c10cc08f984a1ce4dc8caa
unknown <zkfzpf56@naver.com>
2022-04-07 21:20:21 +0900

    Request Body Json Controller

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -56,4 +56,11 @@ public class RequestBodyJsonController {
         log.info("username={}, age={}", helloData.getUsername(),  helloData.getAge());
         return helloData;
     }
+
+    @ResponseBody
+    @PostMapping("/request-body-json-v5")
+    public HelloData requestBodyJsonV5(@RequestBody HelloData data) {
+        log.info("username={}, age={}", data.getUsername(), data.getAge());
+        return data;
+    }
 }
