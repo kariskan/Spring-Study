8ec76e6d5e50020c0c02c928bcd0aacd86aab8f4
unknown <zkfzpf56@naver.com>
2022-04-18 16:58:39 +0900

    apply converter at view templates

--- typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,10 @@ public class HelloController {
         return "ok";
     }
 
-
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort.getIp() = " + ipPort.getIp());
+        System.out.println("ipPort.getPort() = " + ipPort.getPort());
+        return "ok";
+    }
 }
