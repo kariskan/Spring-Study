58e8fe17078a712196af265b9cc5954ea4ebdd88
kariskan <73534426+kariskan@users.noreply.github.com>
2022-04-22 22:02:52 +0900

    JDBC CRUD using H2 database

---
+++ jdbc/src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
@@ -0,0 +1,22 @@
+package hello.jdbc.connection;
+
+import hello.jdbc.repository.MemberRepositoryV0;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+class DBConnectionUtilTest {
+
+    @Test
+    void connection() {
+        Connection connection = DBConnectionUtil.getConnection();
+        assertThat(connection).isNotNull();
+
+    }
+
+}
