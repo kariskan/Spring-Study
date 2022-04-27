a9c2e4f4b52e0ec9b9d2245ff84574ba69d052e3
unknown <zkfzpf56@naver.com>
2022-04-27 16:26:27 +0900

    SpringExceptionTranslator

---
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryEx.java
@@ -0,0 +1,9 @@
+package hello.jdbc.repository;
+import hello.jdbc.domain.Member;
+import java.sql.SQLException;
+public interface MemberRepositoryEx {
+    Member save(Member member);
+    Member findById(String memberId);
+    void update(String memberId, int money);
+    void delete(String memberId);
+}
