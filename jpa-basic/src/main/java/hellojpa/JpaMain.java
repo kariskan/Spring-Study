34c49fb29bbba50b0c346a4df50fbfefae505219
unknown <zkfzpf56@naver.com>
2022-04-29 14:33:54 +0900

    Entity mapping

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -35,12 +34,36 @@ public class JpaMain {
             /**
              * jpql
              */
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
-            for (Member member : result) {
-                System.out.println("member = " + member.getName());
-            }
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .getResultList();
+//            for (Member member : result) {
+//                System.out.println("member = " + member.getName());
+//            }
 
+/*            //비영속
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
+
+            //영속(이 때 DB에 저장되는것은 아니다)
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
+            Member findMember = em.find(Member.class, 101L);
+
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());*/
+//            Member memberA = new Member(1L,"A");
+//            Member memberB = new Member(2L, "B");
+//            System.out.println("persist");
+//
+//            System.out.println("find");
+//            Member findA = em.find(Member.class, 1L);
+//            System.out.println("findA.getName() = " + findA.getName());
+//            System.out.println("findA.getId() = " + findA.getId());
+//
+            System.out.println("commit");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
@@ -48,9 +71,6 @@ public class JpaMain {
             em.close();
         }
         //code
-
         emf.close();
-
-        
     }
 }
--- jpa-basic/src/main/java/hellojpa/Member.java
+++ jpa-basic/src/main/java/hellojpa/Member.java
@@ -1,28 +1,25 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity //jpa를 사용하는 클래스라고 인식
 public class Member {
 
     @Id
     private Long id;
-    private String name;
+    @Column(name = "name")
+    private String username;
+    private Integer age;
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob
+    private String description;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public Member() {
     }
 }
